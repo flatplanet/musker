11c8b8fb4b3debda39992aa5208b7b90d1aa0039
John Elder <johne4196@gmail.com>
2022-12-21 09:23:40 -0800

    Users, Profiles, Follows

--- musker/admin.py
+++ musker/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister Groups
+admin.site.unregister(Group)
+
+# Mix Profile info into User info
+class ProfileInline(admin.StackedInline):
+	model = Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+	model = User
+	# Just display username fields on admin page
+	fields = ["username"]
+	inlines = [ProfileInline]
+
+# Unregister initial User
+admin.site.unregister(User)
+
+# Reregister User and Profile
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
+
--- musker/models.py
+++ musker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create A User Profile Model
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	follows = models.ManyToManyField("self", 
+		related_name="followed_by",
+		symmetrical=False,
+		blank=True)	
+
+	def __str__(self):
+		return self.user.username
+
