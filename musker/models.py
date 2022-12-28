c8c8f7c2267da2d92afffccaaeddba9936c9e16f
John Elder <johne4196@gmail.com>
2022-12-28 10:55:37 -0800

    Automate User Profiles

--- musker/models.py
+++ musker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create A User Profile Model
@@ -12,4 +13,18 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return self.user.username
+
+# Create Profile When New User Signs Up
+#@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		user_profile = Profile(user=instance)
+		user_profile.save()
+		# Have the user follow themselves
+		user_profile.follows.set([instance.profile.id])
+		user_profile.save()
+
+post_save.connect(create_profile, sender=User)
+
+
 
