bd8f3975774b5d14e4b07cee3ca365e47266d226
John Elder <johne4196@gmail.com>
2023-01-11 09:42:06 -0800

    profile page and followers

--- musker/urls.py
+++ musker/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('profile_list/', views.profile_list, name='profile_list'),
+    path('profile/<int:pk>', views.profile, name='profile'),
 ]
--- musker/views.py
+++ musker/views.py
@@ -14,3 +14,11 @@ def profile_list(request):
 	else:
 		messages.success(request, ("You Must Be Logged In To View This Page..."))
 		return redirect('home')
+
+def profile(request, pk):
+	if request.user.is_authenticated:
+		profile = Profile.objects.get(user_id=pk)
+		return render(request, "profile.html", {"profile":profile})
+	else:
+		messages.success(request, ("You Must Be Logged In To View This Page..."))
+		return redirect('home')		
