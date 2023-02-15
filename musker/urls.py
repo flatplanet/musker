9a866c7ef9bf48637da0d36db0e20168fa3028ac
John Elder <johne4196@gmail.com>
2023-02-15 09:22:19 -0800

    Register Users

--- musker/urls.py
+++ musker/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('profile/<int:pk>', views.profile, name='profile'),
     path('login/', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
--- musker/views.py
+++ musker/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Meep
-from .forms import MeepForm
+from .forms import MeepForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 def home(request):
 	if request.user.is_authenticated:
@@ -80,3 +81,22 @@ def logout_user(request):
 	logout(request)
 	messages.success(request, ("You Have Been Logged Out. Sorry to Meep You Go..."))
 	return redirect('home')
+
+def register_user(request):
+	form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			# first_name = form.cleaned_data['first_name']
+			# second_name = form.cleaned_data['second_name']
+			# email = form.cleaned_data['email']
+			# Log in user
+			user = authenticate(username=username, password=password)
+			login(request,user)
+			messages.success(request, ("You have successfully registered! Welcome!"))
+			return redirect('home')
+	
+	return render(request, "register.html", {'form':form})
