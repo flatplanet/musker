198e9a993311d8fd3a508259284348db56db5f0d
John Elder <johne4196@gmail.com>
2023-02-22 09:55:47 -0800

    Update Profiles

--- musker/urls.py
+++ musker/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('update_user/', views.update_user, name='update_user'),
 ]
--- musker/views.py
+++ musker/views.py
@@ -5,6 +5,7 @@ from .forms import MeepForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth.models import User
 
 def home(request):
 	if request.user.is_authenticated:
@@ -100,3 +101,20 @@ def register_user(request):
 			return redirect('home')
 	
 	return render(request, "register.html", {'form':form})
+
+
+def update_user(request):
+	if request.user.is_authenticated:
+		current_user = User.objects.get(id=request.user.id)
+		form = SignUpForm(request.POST or None, instance=current_user)
+		if form.is_valid():
+			form.save()
+			login(request, current_user)
+			messages.success(request, ("Your Profile Has Been Updated!"))
+			return redirect('home')
+
+		return render(request, "update_user.html", {'form':form})
+	else:
+		messages.success(request, ("You Must Be Logged In To View That Page..."))
+		return redirect('home')
+	
