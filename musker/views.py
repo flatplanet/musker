9fc054dfa18668cba9824bf0e4e7893909e39b1a
John Elder <johne4196@gmail.com>
2023-04-12 08:33:01 -0700

    likes on profile pages

--- musker/views.py
+++ musker/views.py
@@ -130,8 +130,8 @@ def meep_like(request, pk):
 			meep.likes.remove(request.user)
 		else:
 			meep.likes.add(request.user)
-
-		return redirect('home')
+		
+		return redirect(request.META.get("HTTP_REFERER"))
 
 
 
